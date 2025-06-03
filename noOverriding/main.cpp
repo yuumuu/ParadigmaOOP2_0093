82d61c501f31860391f4766a224d005e7343b4b4
yuumuu <haidar.alhazen93@gmail.com>
2025-06-03 15:26:40 +0700

    Create Base Class - noOverriding

---
+++ noOverriding/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+    public:
+        virtual void perkenalan() {
+            cout << "Halo, saya Function dari Base Class" << endl;
+        }
+};
