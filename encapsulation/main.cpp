38bf3858a24e32855b7dcf284b374c77f2bc2c58
yuumuu <haidar.alhazen93@gmail.com>
2025-06-03 15:00:14 +0700

    Create Main Function - encapsulation

--- encapsulation/main.cpp
+++ encapsulation/main.cpp
@@ -12,3 +12,19 @@ class RemoteLampu {
             return saklar[i];
         }
 };
+
+int main() {
+    RemoteLampu lampu;
+
+    lampu.setSaklar(0, "Lampu Teras Rumah");
+    lampu.setSaklar(1, "Lampu Ruang Tamu");
+    lampu.setSaklar(2, "Lampu Kamar Tidur");
+    lampu.setSaklar(3, "Lampu Dapur");
+
+    cout << "Saklar 0: " << lampu.getSaklar(0) << endl;
+    cout << "Saklar 1: " << lampu.getSaklar(1) << endl;
+    cout << "Saklar 2: " << lampu.getSaklar(2) << endl;
+    cout << "Saklar 3: " << lampu.getSaklar(3) << endl;
+
+    return 0;
+}
