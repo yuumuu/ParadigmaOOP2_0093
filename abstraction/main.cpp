48fa6119cd607de98b6c1041b00d35d15d4ee948
yuumuu <haidar.alhazen93@gmail.com>
2025-06-03 14:42:41 +0700

    Create Abstraction Class - abstraction

---
+++ abstraction/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Abstraksi {
+    private: string x, y;
+    public:
+        // method untuk mengisi nilai
+        // private member
+        void setVar(string a, string b) {
+            x = a;
+            y = b;
+        }
+        // menampilkan nilai
+        void display() {
+            cout << "x = " << x << endl;
+            cout << "y = " << y << endl;
+        }
+};
