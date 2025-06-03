4e7f41f5a05eb18e2e183fc438c4af594be0a3a1
yuumuu <haidar.alhazen93@gmail.com>
2025-06-03 15:03:26 +0700

    Create Orang Class - MultiInheritance

---
+++ multiInheritance/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class Orang {
+    public:
+        int umur;
+
+        Orang(int pUmur) : umur(pUmur) {
+            cout << "Orang diberi umur " << umur << endl;
+        }
+};
