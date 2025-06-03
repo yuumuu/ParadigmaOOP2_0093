901ec324ca9c2d445e6d44cd946e2d6ae716f54f
yuumuu <haidar.alhazen93@gmail.com>
2025-06-03 14:30:15 +0700

    Create Seseorang Class - polimorphism

---
+++ polimorphism/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+class Seseorang {
+    public:
+        // virtual void pesan() = 0; // pure virtual function
+        virtual void pesan() {
+            cout << "Pesan dari Seseorang" << endl;
+        }
+};
