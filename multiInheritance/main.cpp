e31161d2a6d57db5f1576b4b763fe16d1acc5c16
yuumuu <haidar.alhazen93@gmail.com>
2025-06-03 15:11:29 +0700

    Create Budi Class - multiInheritance

--- multiInheritance/main.cpp
+++ multiInheritance/main.cpp
@@ -23,3 +23,10 @@ class Pelajar : virtual public Orang {
             cout << "Pelajar dibuat" << endl;
         }
 };
+
+class Budi : public Pekerja, public Pelajar {
+    public:
+        Budi(int pUmur) : Pekerja(pUmur), Pelajar(pUmur), Orang(pUmur) {
+            cout << "Budi dibuat" << endl;
+        }
+};
